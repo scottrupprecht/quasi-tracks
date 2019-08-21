1211e45b25f9c4d0675997ddd518b149b946968b
Scott Rupprecht <scott@scottrupprecht.com>
2019-08-20 22:08:28 -0500

    Added github pages deployment + custom domain

--- src/config.js
+++ src/config.js
@@ -4,6 +4,6 @@ export const isProduction = !_.includes(window.location.host, 'localhost');
 
 export default {
   SpotifyClientId: '99f43a6aef114c17bf91c49dfb8469e5',
-  CallbackUri: isProduction ? 'https://slrspotify.z14.web.core.windows.net' : 'http://localhost:3000',
+  CallbackUri: isProduction ? 'https://quacks.xyz' : 'http://localhost:3000',
   Scopes: ['user-read-playback-state', 'playlist-modify-private', 'user-modify-playback-state', 'playlist-read-private'].join(' '),
 };
